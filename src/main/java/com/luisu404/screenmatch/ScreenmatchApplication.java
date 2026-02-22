94a79137191e94c296580c219841e2359e36f50c
Luis Urbaez <luiseduardourbaez@gmail.com>
2026-02-22 01:00:06 -0400

    Final

--- src/main/java/com/luisu404/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/luisu404/screenmatch/ScreenmatchApplication.java
@@ -1,30 +1,20 @@
 package com.luisu404.screenmatch;
 
-import com.luisu404.screenmatch.model.DatosEpisodio;
-import com.luisu404.screenmatch.model.DatosSerie;
-import com.luisu404.screenmatch.model.DatosTemporadas;
-import com.luisu404.screenmatch.principal.EjemploStreams;
 import com.luisu404.screenmatch.principal.Principal;
 import com.luisu404.screenmatch.repository.ISerieRepository;
-import com.luisu404.screenmatch.service.ConsultaGoogleGemini;
-import com.luisu404.screenmatch.service.ConsumoAPI;
-import com.luisu404.screenmatch.service.ConvierteDatos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	@Autowired
+    @Autowired
     private ISerieRepository repository;
     public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
     @Override
     public void run(String... args) throws Exception {
